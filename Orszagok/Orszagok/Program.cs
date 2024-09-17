9ec1bba5e4de22ebb52defb52da51f724c659eb5
csanalosibalint <csanalosib@kkszki.hu>
2024-09-17 14:36:51 +0200

    Orszag osztaly hozzaadasa

---
+++ Orszagok/Orszagok/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orszagok
+{
+    internal class Program
+    {
+        class Orszagok
+        {
+            public string nev;
+            public int terulet;
+            public int nepesseg;
+            public string fovaros;
+            public int fovnepesseg;
+
+            public Orszagok(string sor)
+            {
+                string[] orszagok = sor.Split(';');
+                this.nev = orszagok[0];
+                this.terulet = int.Parse(orszagok[1]);
+                this.nepesseg = int.Parse(orszagok[2]);
+                this.fovaros = orszagok[3];
+                this.fovnepesseg = int.Parse(orszagok[4]);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
